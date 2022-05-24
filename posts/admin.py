ce1529b94b5c2f70c253e18f7ff359739dbe7e22
Mohammad Kazemi Renany <mohammadkazemire9@gmail.com>
2022-05-24 14:09:34 +0430

    part 58

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "created", "image", "edit_time", "slug", "edited"]
+    list_filter = ("created", "edited")
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
-from distutils.command.upload import upload
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+from datetime import datetime
 # Create your models here.
 
 class Post(models.Model):
@@ -11,6 +12,12 @@ class Post(models.Model):
     edited = models.BooleanField(default=False)
     edit_time = models.DateTimeField(auto_now=True)
     slug = models.SlugField(blank=True, max_length=300)
+    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="posts_like", blank=True)
 
     def __str__(self):
-        return self.user+" "+self.created
+        return str(self.user)+" "+str(datetime.now())
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(self.user)+str(datetime.now()))
+        super().save(*args, **kwargs)
