1609fb4fe3ade1f5c4dde846c32351a6ad1007d2
Mohammad Kazemi Renany <43171147+MohamadKazemiRe9@users.noreply.github.com>
2022-06-25 04:03:04 +0430

    Delete myuser directory

--- myuser/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import MyUser
-from django import forms
-
-class UserEditForm(forms.ModelForm):
-    class Meta:
-        model = MyUser
-        fields = ["username", "photo", "first_name", "last_name", "bio"]
--- myuser/urls.py
+++
@@ -1,9 +0,0 @@
-from os import name
-from django.urls import path
-from .views import edit
-
-app_name = "user"
-
-urlpatterns = [
-    path("edit/", edit, name="edit")
-]
